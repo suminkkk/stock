d20361900d0c367fa53092a7474677f39d099291
suminkkk <131845629+suminkkk@users.noreply.github.com>
2023-06-14 23:59:41 +0900

    Update stock_t.py

--- stock_t.py
+++ stock_t.py
@@ -2,29 +2,18 @@ import streamlit as st
 import FinanceDataReader as fdr
 import plotly.graph_objects as go
 
-st.title("주식 차트 시각화 프로젝트")
-st.header("KOSPI")
-st.subheader("www")
+st.title("KOSPI 주식 차트")
+st.subheader("KS11")
 
-def plot_stock_chart(symbol, start_date, end_date):
-    df = fdr.DataReader(symbol, start_date, end_date)
-    df.columns = ['Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
-    fig = go.Figure(data=go.Candlestick(
-                                        open=df['open'],
-                                        high=df['high'],
-                                        low=df['low'],
-                                        close=df['close']))
-    st.plotly_chart(fig)
+# KS11 데이터 가져오기
+df = fdr.DataReader('KS11')
+
+# 캔들스틱 차트 생성
+fig = go.Figure(data=go.Candlestick(x=df.index,
+                                   open=df['Open'],
+                                   high=df['High'],
+                                   low=df['Low'],
+                                   close=df['Close']))
 
 # 차트 출력
-symbol = 'KS11'
-start_date = '2022-05-01'
-end_date = '2023-05-31'
-
-tab1, tab2, tab3 = st.columns(3)
-with tab1:
-    plot_stock_chart(symbol, start_date, end_date)
-with tab2:
-    plot_stock_chart('KS11', '2022-05-01', '2022-05-31')
-with tab3:
-    plot_stock_chart('KS11', '2022-05-02', '2022-05-03')
+st.plotly_chart(fig)
