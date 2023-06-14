d7dbcf3cbfadef2fce0b802c3a1b6841f57b53a4
suminkkk <smin.geem@gmail.com>
2023-06-14 21:49:01 +0900

    주식배포

---
+++ stock_t.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import FinanceDataReader as fdr
+import plotly.graph_objects as go
+import pandas as pd
+import os
+
+
+st.title("주식 차트 시각화 프로젝트")
+st.header("KOSPI")
+st.subheader("www")
+
+def plot_stock_chart(symbol, start_date, end_date):
+    df = fdr.DataReader(symbol, start_date, end_date)
+    
+    fig = go.Figure(data=go.Candlestick(x=df.index,
+                                        open=df['Open'],
+                                        high=df['High'],
+                                        low=df['Low'],
+                                        close=df['Close']))
+    st.plotly_chart(fig)
+
+
+# 차트 출력 
+symbol = 'ks11'  
+start_date = '2022-05-01'
+end_date = '2023-05-31'
+
+
+tab1, tab2, tab3 = st.tabs(['년' , '월', '일'])
+with tab1:
+    plot_stock_chart(symbol, start_date, end_date)
+with tab2:
+    plot_stock_chart('ks11','2022-05-01','2022-05-31')
+with tab3:
+    df = pd.read_excel(os.getcwd()+'2008_2022_연령별인구현황_월간.xlsx')
+    st.dataframe(df)
