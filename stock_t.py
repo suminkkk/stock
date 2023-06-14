eae7c8e326b4b5d2840f360e129cecf9961c002c
suminkkk <131845629+suminkkk@users.noreply.github.com>
2023-06-15 00:39:58 +0900

    Update stock_t.py

--- stock_t.py
+++ stock_t.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import FinanceDataReader as fdr
 import plotly.graph_objects as go
-import pandas as pd
 
 st.title("KOSPI 주식 차트")
 st.subheader("KS11")
@@ -9,12 +8,13 @@ st.subheader("KS11")
 # KS11 데이터 가져오기
 df = fdr.DataReader('KS11')
 
-# 인덱스를 날짜 형식으로 변환
-df.index = pd.to_datetime(df.index)
+# 데이터프레임의 인덱스를 날짜 열로 변경
+df = df.reset_index()
+
 
 # 캔들스틱 차트 생성
-fig = go.Figure(data=go.Candlestick(x=df.index,
-                                   open=df['Close'],
+fig = go.Figure(data=go.Candlestick(x=df['Date'],
+                                   open=df['Open'],
                                    high=df['High'],
                                    low=df['Low'],
                                    close=df['Close']))
