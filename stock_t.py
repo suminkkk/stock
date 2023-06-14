3442516dc227e18be0716220dd8a6e4dce95df91
suminkkk <131845629+suminkkk@users.noreply.github.com>
2023-06-15 00:05:04 +0900

    Update stock_t.py

--- stock_t.py
+++ stock_t.py
@@ -7,10 +7,12 @@ st.subheader("KS11")
 
 # KS11 데이터 가져오기
 df = fdr.DataReader('KS11')
-print(df.head())
+
+# 데이터프레임의 인덱스를 날짜 열로 변경
+df = df.reset_index()
+
 # 캔들스틱 차트 생성
-fig = go.Figure(data=go.Candlestick(x=df.index,
-                                    date = df['Date'],
+fig = go.Figure(data=go.Candlestick(x=df['Date'],
                                    open=df['Open'],
                                    high=df['High'],
                                    low=df['Low'],
