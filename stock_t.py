74f7e33a3f2afe41afa59fa0b2638173f9929e29
suminkkk <131845629+suminkkk@users.noreply.github.com>
2023-06-14 22:28:57 +0900

    Update stock_t.py

--- stock_t.py
+++ stock_t.py
@@ -2,6 +2,5 @@ import streamlit as st
 import pandas as pd
 import os
 
-
-    df = pd.read_excel(os.getcwd()+'2008_2022_연령별인구현황_월간.xlsx')
-    st.dataframe(df)
+df = pd.read_excel(os.getcwd()+'2008_2022_연령별인구현황_월간.xlsx')
+st.dataframe(df)
