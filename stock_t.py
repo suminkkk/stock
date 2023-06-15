ce67cb8362a63e9f1ffa74ce6e9407c6ae0c1d28
suminkkk <131845629+suminkkk@users.noreply.github.com>
2023-06-15 18:04:15 +0900

    Update stock_t.py

--- stock_t.py
+++ stock_t.py
@@ -1,39 +1,68 @@
 import streamlit as st
 import FinanceDataReader as fdr
 import plotly.graph_objects as go
+import requests
+from bs4 import BeautifulSoup
 
-# 제목
-st.title("[주식 차트 대시보드]")
-st.title("KOSPI")
-st.subheader("KS11")
+def main():
+    # 제목
+    st.title("[주식 차트 대시보드]")
+    st.title("KOSPI")
+    st.subheader("KS11")
 
-# KS11 데이터 가져오기
-df = fdr.DataReader('KS11')
-df.reset_index(inplace=True) 
+    symbol = 'KS11'
 
-# 데이터프레임의 인덱스를 날짜 열로 변경
-df = df.reset_index()
+    # 사용자로부터 시작 날짜와 종료 날짜 입력 받기
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input("시작 날짜")
+    with col2:
+        end_date = st.date_input("종료 날짜")
 
-# 캔들스틱 차트 생성
-def plot_stock_chart(symbol, start_date, end_date):
-    df = fdr.DataReader(symbol, start_date, end_date)
-    fig = go.Figure(data=go.Candlestick(x=df.index,
-                                   open=df['Open'],
-                                   high=df['High'],
-                                   low=df['Low'],
-                                   close=df['Close']))
-    st.plotly_chart(fig)
+    # 날짜를 문자열로 변환
+    start_date_str = start_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
 
-# 차트 출력 
-symbol = 'ks11'  
-start_date = '2022-05-01'
-end_date = '2023-05-31'
+    # 캔들스틱 차트 생성
+    @st.cache_data
+    def plot_stock_chart(symbol, start_date, end_date):
+        df = fdr.DataReader(symbol, start_date, end_date)
+        fig = go.Figure(data=go.Candlestick(x=df.index,
+                                       open=df['Open'],
+                                       high=df['High'],
+                                       low=df['Low'],
+                                       close=df['Close']))
+        st.plotly_chart(fig)
+
+    # 차트 출력
+    plot_stock_chart(symbol, start_date_str, end_date_str)
+
+    # 날짜 하드코딩
+    tab1, tab2, tab3, tab4 = st.tabs(['1일' , '3개월', '1년','3년'])
+    with tab1:
+        plot_stock_chart(symbol,'2022-05-03','2022-05-04')
+    with tab2:
+        plot_stock_chart(symbol,'2022-05-01','2022-07-31')
+    with tab3:
+        plot_stock_chart(symbol,'2021-05-01','2022-05-31')
+    with tab4:
+        plot_stock_chart(symbol, '2020-05-01','2022-05-31')
+
+def get_popular_search_keywords():
+    url = 'https://finance.naver.com/sise/sise_index.naver?code=KOSPI'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    keyword_elements = soup.select('.type_r1')
+    keywords = [keyword.text for keyword in keyword_elements]
+    st.text(keywords)
+    
+    return keywords[:5]
+    
 
 
-tab1, tab2, tab3 = st.tabs(['년' , '월', '일'])
-with tab1:
-    plot_stock_chart(symbol, start_date, end_date)
-with tab2:
-    plot_stock_chart('ks11','2022-05-01','2022-05-31')
-with tab3:
-    plot_stock_chart('ks11','2022-05-02','2022-05-03')
+
+if __name__ == '__main__':
+    main()
+    popular_keywords = get_popular_search_keywords()
+    for i, keyword in enumerate(popular_keywords, 1):
+        print(f"{i}. {keyword}")
